25212364608e5366abab2daa83522389a117ceb6
Sasha Laundy <sasha.laundy@gmail.com>
2020-12-14 22:48:24 -0800

    Add comments

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ DICT_FILENAME = 'dictionary.txt'
 
 
 def load_dictionary(filename):
+    ''' Load a file containing all possible words. '''
     with open(filename, 'r') as dictfile:
         dict_words = list(dictfile)
     dict_words = list(map(str.strip, dict_words))
@@ -13,7 +14,7 @@ def load_dictionary(filename):
 
 
 def load_letters(l):
-    ''' Assume first letter is the center letter '''
+    ''' Load today's letters. Assume first letter is the center letter '''
     center_letter = l[0]
     assert len(set(l)) == 7
     return center_letter, set(l)
@@ -21,12 +22,14 @@ def load_letters(l):
 
 if __name__ == '__main__':
 
+    # Set up
     parser = argparse.ArgumentParser(description='Process today\'s letters')
     parser.add_argument('letters')
-
     dictionary = load_dictionary(DICT_FILENAME)
     center_letter, letters = load_letters(parser.parse_args().letters)
 
+    # Go through each dictionary word, seeing if it's a match!
+    # Highlight any potential pangrams
     for word in dictionary:
         if center_letter in word and set(word).issubset(letters):
             if set(word) == set(letters):
