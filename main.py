0f0f5a67c2fad5609a0d87deb84e783ac725716a
Sasha Laundy <sasha.laundy@gmail.com>
2020-12-14 22:38:26 -0800

    Get letters with args

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import argparse
+
+
+DICT_FILENAME = 'dictionary.txt'
+
+
 def load_dictionary(filename):
     with open(filename, 'r') as dictfile:
-        dict_words = list(dictfile).strip().lower()
-    return dict_words
+        dict_words = list(dictfile)
+    dict_words = list(map(str.strip, dict_words))
+    dict_words = list(map(str.lower, dict_words))
+    return filter(lambda x: len(x) > 3, dict_words)
 
 
 def load_letters(l):
@@ -13,11 +21,11 @@ def load_letters(l):
 
 if __name__ == '__main__':
 
-    dict_filename = 'dictionary.txt'
-    dictionary = load_dictionary(dict_filename)
+    parser = argparse.ArgumentParser(description='Process today\'s letters')
+    parser.add_argument('letters')
 
-    # TODO load these with args
-    center_letter, letters = load_letters('hlucant')
+    dictionary = load_dictionary(DICT_FILENAME)
+    center_letter, letters = load_letters(parser.parse_args().letters)
 
     for word in dictionary:
         if center_letter in word and set(word).issubset(letters):
