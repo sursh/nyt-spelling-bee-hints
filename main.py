737367ee13254b0c7023a0dceffa2655127f7d26
Sasha Laundy <sasha.laundy@gmail.com>
2020-12-16 18:36:32 -0800

    Connect the real answers to hints

--- main.py
+++ main.py
@@ -16,6 +16,25 @@ def load_dictionary(filename):
     return filter(lambda x: len(x) > 3, dict_words)
 
 
+def parse_cli_letters():
+    # Get letters from CLI args
+    parser = argparse.ArgumentParser(description='Process today\'s letters')
+    parser.add_argument('letters')
+    center_letter, letters = load_letters(parser.parse_args().letters)
+    return center_letter, letters
+
+
+def find_dictionary_matches(dictionary, center_letter, letters):
+    # Go through each dictionary word, seeing if it's a match!
+    # Highlight any potential pangrams
+    for word in dictionary:
+        if center_letter in word and set(word).issubset(letters):
+            if set(word) == set(letters):
+                print(word, '  <- PANGRAM')
+            else:
+                print(word)
+
+
 def load_letters(l):
     ''' Load today's letters. Assume first letter is the center letter '''
     center_letter = l[0]
@@ -47,27 +66,37 @@ def get_letters(day='today'):
     return puzzle[day]['centerLetter'], puzzle[day]['validLetters']
 
 
+def query_datamuse(word, endpoint='http://api.datamuse.com/words',
+                   query_type='ml'):
+    url = endpoint + "?" + query_type + "=" + word
+    return requests.get(url).json()
+
+
+def get_related_word(word, n=1, query_type='ml'):
+    """ Example: lately https://api.datamuse.com/words?ml=lately """
+    results = query_datamuse(word, query_type=query_type)
+    subset = results[:n]
+    return [entry['word'] for entry in subset]
+
+
+def format_multiple_word_hint(hints):
+    return ', '.join(hints)
+
+
 if __name__ == '__main__':
 
     # Load words
     dictionary = load_dictionary(DICT_FILENAME)
 
-    # Get letters from CLI args
-    parser = argparse.ArgumentParser(description='Process today\'s letters')
-    parser.add_argument('letters')
-    center_letter, letters = load_letters(parser.parse_args().letters)
-
-    # Go through each dictionary word, seeing if it's a match!
-    # Highlight any potential pangrams
-    for word in dictionary:
-        if center_letter in word and set(word).issubset(letters):
-            if set(word) == set(letters):
-                print(word, '  <- PANGRAM')
-            else:
-                print(word)
-
     # Get the letters from the actual puzzle
-    center_letter, all_letters = get_letters()
+    center_letter, letters = get_letters()
 
     # Get the answers from the actual puzzle
     pangrams, answers = get_answers()
+
+    # Give a hint for each word
+    for answer in answers:
+        # print(answer, get_related_word(answer, 3))
+        print("Hint: ",
+              format_multiple_word_hint(get_related_word(answer, 3)),
+              "(", answer, ")")
